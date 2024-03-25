29e2b2b941f513e49dd89e58a630a77242809f5b
BbeatrizSI <bbeatriz@gmail.com>
2024-03-25 18:34:22 +0100

    adjunto html

--- src/main/java/com/example/ControladorREST.java
+++ src/main/java/com/example/ControladorREST.java
@@ -1,16 +1,16 @@
 package com.example;
 
-import ch.qos.logback.classic.Logger;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorREST {
-    @GetMapping("/")
-    public String start(Logger log) {
-        log.info("Estoy ejecutando el controlador REST");
-        return "Mi primer Hola Mundo";
+
+    @GetMapping("")
+    public String start() {
+        log.info("Estoy ejecutando MVN");
+        return "indice.html";
     }
 }
